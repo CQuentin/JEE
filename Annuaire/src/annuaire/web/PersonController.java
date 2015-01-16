a8cdf46881e76143a7d41b7b37e92459ae508356
dEsk <desk@desk-CX61-2PC.(none)>
2015-01-16 16:14:01 +0100

    Sécurisation des pages

--- Annuaire/src/annuaire/web/PersonController.java
+++ Annuaire/src/annuaire/web/PersonController.java
@@ -19,108 +19,125 @@ public class PersonController {
 
 	@Autowired()
 	User user;
-	
+
 	@Autowired
-    IDAOPerson daoPerson;
+	IDAOPerson daoPerson;
 	
 	@RequestMapping(value = "/annuaire.htm")
-    public String list() {
-        return "listPerson";
-    }
+	public String list() {
+		if(!user.isConnected()) {
+			return "redirect:/auth/login.htm";
+		}
+		return "listPerson";
+	}
 
-    @ModelAttribute("persons")
-    public Collection<Person> persons() {
-        return daoPerson.findAllPersons();
-    }
-    
-    @RequestMapping(value = "/detail.htm", method = RequestMethod.GET)
-    public String detailPerson(@ModelAttribute Person p) {
-    	
-    	if(p.getLogin().isEmpty()) {
-    		return "redirect:annuaire.htm";
-    	}
-        return "person";
-    }
-    
-    @ModelAttribute("user")
-    public User newUser() {
-        return user;
-    }
-    
-    @ModelAttribute("person")
-    public Person newPerson(
-            @RequestParam(value = "id", required = false) String personLogin) {
-        
-    	if (personLogin != null) {
-            return daoPerson.findPerson(personLogin);
-        }
-    	
-        Person p = new Person();
-        p.setLogin("");
-        p.setLastName("");
-        p.setFirstName("");
-        p.setMail("");
-        p.setWebsite("");
-        p.setDateOfBirth(null);
-        p.setPassword("");
-        return p;
-    }
-  
-    @RequestMapping(value = "/edition.htm", method = RequestMethod.GET)
-    public String editPerson(@ModelAttribute Person p) {
-    	if(p.getLogin().isEmpty()) {
-    		return "redirect:annuaire.htm";
-    	}
-    	
-    	if(!p.getLogin().equals(user.getLogin())) {
-    		return "redirect:annuaire.htm";
-    	}
-    	
-    	return "personForm";
-    }
-    
-    @RequestMapping(value = "/edition.htm", method = RequestMethod.POST)
-    public String savePerson(@ModelAttribute Person p, BindingResult result) {
-    	
-    	if(result.hasErrors()) {
-    		return "personForm";
-    	}
-    	
-    	if(!p.getLogin().equals(user.getLogin())) {
-    		return "redirect:annuaire.htm";
-    	}
-    	
-    	daoPerson.savePerson(p);
-    	
-    	return "redirect:detail.htm?id=" + p.getLogin();
-    }
-    
-    @RequestMapping(value = "/add.htm")
-    public String addPerson() {
-    	return "personAddition";
-    }
-    
-    @RequestMapping(value = "/add.htm", method = RequestMethod.POST)
-    public String saveNewPerson(@ModelAttribute Person p, BindingResult result) {
-    	
-    	if(result.hasErrors()) {
-    		return "personAddition";
-    	}
-    	
-    	daoPerson.addPerson(p);
-    	
-    	return "redirect:detail.htm?id=" + p.getLogin();
-    }
-    
-    @RequestMapping(value = "/delete.htm", method = RequestMethod.GET)
-    public String deletePerson(@ModelAttribute Person p) {
-    	
-    	if(p.getLogin().isEmpty()) {
-    		return "redirect:annuaire.htm";
-    	}
-    	
-    	daoPerson.deletePerson(p.getLogin());
-    	
-    	return "redirect:annuaire.htm";
-    }
+	@ModelAttribute("persons")
+	public Collection<Person> persons() {
+		return daoPerson.findAllPersons();
+	}
+
+	@RequestMapping(value = "/detail.htm", method = RequestMethod.GET)
+	public String detailPerson(@ModelAttribute Person p) {
+		if(!user.isConnected()) {
+			return "redirect:/auth/login.htm";
+		}
+		if(p.getLogin().isEmpty()) {
+			return "redirect:annuaire.htm";
+		}
+		return "person";
+	}
+
+	@ModelAttribute("user")
+	public User newUser() {
+		return user;
+	}
+
+	@ModelAttribute("person")
+	public Person newPerson(
+			@RequestParam(value = "id", required = false) String personLogin) {
+
+		if (personLogin != null) {
+			return daoPerson.findPerson(personLogin);
+		}
+
+		Person p = new Person();
+		p.setLogin("");
+		p.setLastName("");
+		p.setFirstName("");
+		p.setMail("");
+		p.setWebsite("");
+		p.setDateOfBirth(null);
+		p.setPassword("");
+		return p;
+	}
+
+	@RequestMapping(value = "/edition.htm", method = RequestMethod.GET)
+	public String editPerson(@ModelAttribute Person p) {
+		if(!user.isConnected()) {
+			return "redirect:/auth/login.htm";
+		}
+		if(p.getLogin().isEmpty()) {
+			return "redirect:annuaire.htm";
+		}
+
+		if(!p.getLogin().equals(user.getLogin())) {
+			return "redirect:annuaire.htm";
+		}
+
+		return "personForm";
+	}
+
+	@RequestMapping(value = "/edition.htm", method = RequestMethod.POST)
+	public String savePerson(@ModelAttribute Person p, BindingResult result) {
+		if(!user.isConnected()) {
+			return "redirect:/auth/login.htm";
+		}
+		if(result.hasErrors()) {
+			return "personForm";
+		}
+
+		if(!p.getLogin().equals(user.getLogin())) {
+			return "redirect:annuaire.htm";
+		}
+
+		daoPerson.savePerson(p);
+
+		return "redirect:detail.htm?id=" + p.getLogin();
+	}
+
+	@RequestMapping(value = "/add.htm")
+	public String addPerson() {
+		if(!user.isConnected()) {
+			return "redirect:/auth/login.htm";
+		}
+		return "personAddition";
+	}
+
+	@RequestMapping(value = "/add.htm", method = RequestMethod.POST)
+	public String saveNewPerson(@ModelAttribute Person p, BindingResult result) {
+		if(!user.isConnected()) {
+			return "redirect:/auth/login.htm";
+		}
+		if(result.hasErrors()) {
+			return "personAddition";
+		}
+
+		daoPerson.addPerson(p);
+
+		return "redirect:detail.htm?id=" + p.getLogin();
+	}
+
+	@RequestMapping(value = "/delete.htm", method = RequestMethod.GET)
+	public String deletePerson(@ModelAttribute Person p) {
+		if(!user.isConnected()) {
+			return "redirect:/auth/login.htm";
+		}
+		if(p.getLogin().isEmpty()) {
+			return "redirect:annuaire.htm";
+		}
+
+		daoPerson.deletePerson(p.getLogin());
+
+		return "redirect:annuaire.htm";
+	}
 }
