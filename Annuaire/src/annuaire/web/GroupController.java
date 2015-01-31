7672fa1d2f4a8f0b491411382a7103037b7117d6
Quentin <cheynet.q@gmail.com>
2015-01-31 21:01:19 +0100

    ajout des tags pour la javadoc

--- Annuaire/src/annuaire/web/GroupController.java
+++ Annuaire/src/annuaire/web/GroupController.java
@@ -18,9 +18,16 @@ import annuaire.model.Group;
 import annuaire.model.Person;
 import annuaire.services.IDAOGroup;
 
+/**
+* GroupController le controller de Group
+*
+* @author Quentin Cheynet
+* Yoann Moisset
+*/
+
 @Controller()
 @RequestMapping("/group")
-public class GroupController { //TODO gerer interceptor
+public class GroupController {
 
 	@Autowired()
 	User user;
@@ -28,16 +35,29 @@ public class GroupController { //TODO gerer interceptor
 	@Autowired
 	IDAOGroup daoGroup;
 
+	/**
+	 * 
+	 * @return le nom du fichier listGroup.jspx qui affiche tous les groupes existant
+	 */
 	@RequestMapping(value = "/groups.htm")
 	public String list() {
 		return "listGroup";
 	}
 
+	/**
+	 * 
+	 * @return une collection de Group via le DAO à partir la table MYGROUP de la base de donnée
+	 */
 	@ModelAttribute("groups")
 	public Collection<Group> groups() {
 		return daoGroup.findAllGroups();
 	}
 
+	/**
+	 * 
+	 * @param groupname Le nom du Group de la base de donnée que l'on veut renvoyer
+	 * @return Group, avec les attributs correspondant au groupe de la base de donnée
+	 */
 	@ModelAttribute("group")
 	public Group newGroup(
 			@RequestParam(value = "id", required = false) String groupname) {
@@ -51,6 +71,12 @@ public class GroupController { //TODO gerer interceptor
 		return g;
 	}
 
+	/**
+	 * Donne l'accès à la modification d'un groupe si on en a les droits
+	 * @param g Le groupe g dont on veut éditer les détails
+	 * @return	- le nom du fichier groupForm.jspx si on est un administrateur
+	 * 			- une redirection vers la liste des groupes sinon
+	 */
 	@RequestMapping(value = "/edition.htm", method = RequestMethod.GET)
 	public String editGroup(@ModelAttribute Group g) {
 
@@ -65,6 +91,13 @@ public class GroupController { //TODO gerer interceptor
 
 	}
 
+	/**
+	 * Modifie des détails d'un groupe via le DAO si on en a les droits
+	 * @param g Le groupe g dont on veut éditer les détails
+	 * @param result
+	 * @return	- une redirection vers la liste des groupes
+	 *			- le nom du fichier groupForm.jspx si result indique qu'il y a des erreurs ou si le nom du groupe existe déjà
+	 */
 	@RequestMapping(value = "/edition.htm", method = RequestMethod.POST)
 	public ModelAndView saveGroup(@ModelAttribute @Valid Group g, BindingResult result) {
 		if(result.hasErrors()) {
@@ -85,6 +118,11 @@ public class GroupController { //TODO gerer interceptor
 
 	}
 
+	/**
+	 * Donne l'accès à l'ajout d'un groupe si on en a les droits
+	 * @return	- le nom du fichier groupAddition.jspx si on est un administrateur
+	 *			- une redirection vers la liste des personnes sinon
+	 */
 	@RequestMapping(value = "/add.htm", method = RequestMethod.GET)
 	public String addGroup() {
 		if (user.isAdmin()){
@@ -93,6 +131,13 @@ public class GroupController { //TODO gerer interceptor
 		return "redirect:groups.htm";
 	}
 
+	/**
+	 * Ajoute une personne via le DAO si on en a les droits 
+	 * @param g Le groupe g que l'on veut ajouter
+	 * @param result Le résultat de la requête, pour savoir si elle contient des erreur
+	 * @return	- une redirection vers la liste des groupes
+	 * 			- le nom du fichier groupAddition.jspx si result indique qu'il y a des erreurs ou si le groupe existe déjà
+	 */
 	@RequestMapping(value = "/add.htm", method = RequestMethod.POST)
 	public ModelAndView saveNewGroup(@ModelAttribute @Valid Group g, BindingResult result) {
 
@@ -109,6 +154,11 @@ public class GroupController { //TODO gerer interceptor
 		return new ModelAndView("redirect:groups.htm");
 	}
 
+	/**
+	 * Supprimer un groupe via le DAO si on en a les droits 
+	 * @param g Le groupe g que l'on veut supprimer
+	 * @return Une redirection vers la liste des groupes
+	 */
 		@RequestMapping(value = "/delete.htm", method = RequestMethod.GET)
 		public String deleteGroup(@ModelAttribute Group g) {
 
@@ -121,10 +171,13 @@ public class GroupController { //TODO gerer interceptor
 			}
 			return "redirect:groups.htm";
 		}
-
+		
+		/**
+		 *  créé et retourne un User
+		 * @return Le User
+		 */
 		@ModelAttribute("user")
 		public User newUser() {
 			return user;
 		}
-
 	}
--- Annuaire/src/annuaire/web/PersonController.java
+++ Annuaire/src/annuaire/web/PersonController.java
@@ -24,6 +24,13 @@ import annuaire.model.Person;
 import annuaire.services.IDAOGroup;
 import annuaire.services.IDAOPerson;
 
+/**
+* PersonController le controller de Person
+*
+* @author Quentin Cheynet
+* Yoann Moisset
+*/
+
 @Controller()
 @RequestMapping("/person")
 public class PersonController {
@@ -37,27 +44,49 @@ public class PersonController {
 	@Autowired
 	IDAOGroup daoGroup;
 
+	/**
+	 * 
+	 * @return le nom du fichier listPerson.jspx qui affiche toutes les personnes de l'annuaire
+	 */
 	@RequestMapping(value = "/annuaire.htm")
 	public String list() {
 		return "listPerson";
 	}
 
+	/**
+	 * 
+	 * @return une collection de Person via le DAO à partir la table PERSON de la base de donnée
+	 */
 	@ModelAttribute("persons")
 	public Collection<Person> persons() {
 		return daoPerson.findAllPersons();
 	}
 
+	/**
+	 * 
+	 * @param p La personne p dont on veut voir les détails
+	 * @return le nom du fichier person.jspx qui affiche les détails de la personne p
+	 */
 	@RequestMapping(value = "/detail.htm", method = RequestMethod.GET)
 	public String detailPerson(@ModelAttribute Person p) {
 
 		return "person";
 	}
 
+	/**
+	 *  créé et retourne un User
+	 * @return Le User
+	 */
 	@ModelAttribute("user")
 	public User newUser() {
 		return user;
 	}
 
+	/**
+	 * 
+	 * @param personLogin Le login de la Person de la base de donnée que l'on veut renvoyer
+	 * @return Person, avec les attributs correspondant à la personne de la base de donnée 
+	 */
 	@ModelAttribute("person")
 	public Person newPerson(
 			@RequestParam(value = "id", required = false) String personLogin) {
@@ -78,6 +107,12 @@ public class PersonController {
 		return p;
 	}
 
+	/**
+	 * Donne l'accès à la modification des détails d'une personne si on en a les droits
+	 * @param p La personne p dont on veut éditer les détails
+	 * @return 	- le nom du fichier personForm.jspx si on est un administrateur ou la personne à modifier
+	 *			- une redirection vers la liste des personnes sinon
+	 */
 	@RequestMapping(value = "/edition.htm", method = RequestMethod.GET)
 	public String editPerson(@ModelAttribute Person p) {
 
@@ -91,6 +126,14 @@ public class PersonController {
 		return "redirect:annuaire.htm";		
 	}
 
+	/**
+	 * Modifie des détails d'une personne via le DAO si on en a les droits
+	 * @param p La personne p dont on veut éditer les détails
+	 * @param result Le résultat de la requête, pour savoir si elle contient des erreur
+	 * @return 	- une redirection vers les détails de la personne modifié si on est un administrateur ou la personne modifiée
+	 *			- une redirection vers la liste des personnes sinon
+	 *			- le nom du fichier personForm.jspx si result indique qu'il y a des erreurs
+	 */
 	@RequestMapping(value = "/edition.htm", method = RequestMethod.POST)
 	public String savePerson(@ModelAttribute @Valid Person p, BindingResult result) {
 		if(result.hasErrors()) {
@@ -117,6 +160,11 @@ public class PersonController {
 		return "redirect:annuaire.htm";
 	}
 
+	/**
+	 * Donne l'accès à l'ajout d'une personne si on en a les droits
+	 * @return 	- le nom du fichier personAddition.jspx si on est un administrateur
+	 *			- une redirection vers la liste des personnes sinon
+	 */
 	@RequestMapping(value = "/add.htm", method = RequestMethod.GET)
 	public String addPerson() {
 		if (user.isAdmin()){
@@ -125,6 +173,14 @@ public class PersonController {
 		return "redirect:annuaire.htm";
 	}
 
+	/**
+	 * Ajoute une personne via le DAO si on en a les droits 
+	 * @param p La personne p que l'on veut ajouter
+	 * @param result Le résultat de la requête, pour savoir si elle contient des erreur
+	 * @return 	- une redirection vers les détails de la personne ajouté si on est un administrateur
+	 * 			- une redirection vers la liste des personnes sinon
+	 * 			- le nom du fichier personForm.jspx si result indique qu'il y a des erreurs ou si la personne existe déjà
+	 */
 	@RequestMapping(value = "/add.htm", method = RequestMethod.POST)
 	public ModelAndView saveNewPerson(@ModelAttribute @Valid Person p, BindingResult result) {
 
@@ -133,9 +189,6 @@ public class PersonController {
 		}
 
 		if (user.isAdmin()){
-			
-
-				
 			if(p.getGroups() == null 
 					|| p.getGroups().isEmpty() 
 					|| p.getGroups().iterator().next() == null
@@ -153,6 +206,11 @@ public class PersonController {
 		return new ModelAndView("redirect:annuaire.htm");
 	}
 
+	/**
+	 * Supprimer une personne via le DAO si on en a les droits 
+	 * @param p La personne p que l'on veut supprimer
+	 * @return Une redirection vers la liste des personnes
+	 */
 	@RequestMapping(value = "/delete.htm", method = RequestMethod.GET)
 	public String deletePerson(@ModelAttribute Person p) {
 
@@ -168,14 +226,20 @@ public class PersonController {
 	}
 
 
+	/**
+	 * Créé une liste de tous les groupes existant via le DAO, et la renvoie
+	 * @return la liste de tous les groupes existant
+	 */
 	@ModelAttribute("groupsList")
 	public List<Group> productTypes() {
-
 		return (List<Group>) daoGroup.findAllGroups();
 	}
 
-
-
+	/**
+	 * Permet de récupérer la liste des groupes séléctionnés via un formulaire,
+	 * et de la renvoyer sous un type exploitable
+	 * @param binder
+	 */
 	@InitBinder
 	protected void initBinder(WebDataBinder binder) {
 		binder.registerCustomEditor(Set.class, "groups", new CustomCollectionEditor(Set.class)
